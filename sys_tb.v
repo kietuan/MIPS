d40d79ef74f7679268c7e149493d6a4be094f7be
tuankiet <tuankiet0303@gmail.com>
2023-05-10 17:18:30 +0700

    for test

--- sys_tb.v
+++ sys_tb.v
@@ -26,42 +26,49 @@ module sys_tb();
 
     reg [2:0]  SYS_output_sel; //trong �'�? l�  7 bit nhưng chỉ cần 3 bit l�  �'ủ hiện thực
     
-    wire [7:0] F_PC;
-    wire [31:0] F_instruction;
-    wire [31:0] D_instruction;
-    wire [31:0] EX_instruction, MEM_instruction, WB_instruction;
-    
+    // wire [7:0] F_PC;
+    wire [31:0] F_instruction, D_instruction, EX_instruction, MEM_instruction, WB_instruction;
+    // wire D_stall;
+    // wire [1:0] MEM_to_D_forwardSignal;
+
     wire        WB_RegWrite_signal;
-    wire [4:0]  WB_write_register;
     wire [31:0] WB_write_data;
-    
-    wire [2:0]EX_exception_signal;
-    wire [31:0] EX_a_operand, EX_b_operand;
+    wire [4:0]  WB_write_register;
+    wire [2:0]  WB_exception_signal;
+    wire [10:0] EX_control_signal;
+    // wire [4:0] D_Reg_address1 = D_instruction[25:21], D_Reg_address2 = D_instruction[20:16];
+    // wire [31:0] D_REG_data_out1, D_REG_data_out2;
     reg [31:0] testt_reg_add;
     wire [31:0]testt_reg;
 
     //test
    system sy(
         //INPUT, giữ nguyên
-        .SYS_clk(SYS_clk),
-        .SYS_reset(SYS_reset),
-        .SYS_output_sel(SYS_output_sel), //trong �'�? l�  7 bit nhưng chỉ cần 3 bit l�  �'ủ hiện thực
-        .testt_reg_add(testt_reg_add),
+        .SYS_clk                (SYS_clk),
+        .SYS_reset              (SYS_reset),
+        .SYS_output_sel         (SYS_output_sel), //trong �'�? l�  7 bit nhưng chỉ cần 3 bit l�  �'ủ hiện thực
+        .testt_reg_add          (testt_reg_add),
 
         //OUTPUT
-        .testt_reg(testt_reg),
-        .PC (F_PC),
-        .F_instruction(F_instruction),
-        .D_instruction(D_instruction),
-        .EX_instruction(EX_instruction),
-        .MEM_instruction(MEM_instruction),
-        .WB_instruction(WB_instruction),
-        .EX_exception_signal(EX_exception_signal),
-        .EX_a_operand(EX_a_operand),
-        .EX_b_operand(EX_b_operand),
-        .WB_RegWrite_signal(WB_RegWrite_signal),
-        .WB_write_register(WB_write_register),
-        .WB_write_data(WB_write_data)
+        // .PC (F_PC),
+        .F_instruction          (F_instruction),
+        .D_instruction          (D_instruction),
+        .EX_instruction         (EX_instruction),
+        .MEM_instruction        (MEM_instruction),
+        .WB_instruction         (WB_instruction),
+
+        // .D_stall                (D_stall),
+        .WB_RegWrite_signal     (WB_RegWrite_signal),
+        .WB_write_data          (WB_write_data),
+        .WB_write_register      (WB_write_register),
+        .WB_exception_signal    (WB_exception_signal),
+        .EX_control_signal      (EX_control_signal),
+        // .D_REG_data_out1        (D_REG_data_out1),
+        // .D_REG_data_out2        (D_REG_data_out2),
+        // .MEM_to_D_forwardSignal (MEM_to_D_forwardSignal),
+
+        .testt_reg              (testt_reg)
+
     );
     initial begin
         SYS_clk=0;
@@ -71,10 +78,11 @@ module sys_tb();
     initial
         begin
              //ki?m tra gi? tr? thanh ghi s? 8
-            testt_reg_add = 9;
+            testt_reg_add = 8;
             SYS_reset = 0;
             SYS_output_sel = 2;
             #1 SYS_reset = 1;
             #2 SYS_reset = 0;
         end 
+    initial #250 $finish;
 endmodule
