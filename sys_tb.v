efaf418444c70125c754e5e39bf33d361ad023a1
tuankiet <tuankiet0303@gmail.com>
2023-05-12 08:24:56 +0700

    sửa lại testbench

--- sys_tb.v
+++ sys_tb.v
@@ -27,48 +27,19 @@ module sys_tb();
     reg [2:0]  SYS_output_sel; //trong �'�? l�  7 bit nhưng chỉ cần 3 bit l�  �'ủ hiện thực
     
     // wire [7:0] F_PC;
-    wire [31:0] F_instruction, D_instruction, EX_instruction, MEM_instruction, WB_instruction;
-    // wire D_stall;
-    // wire [1:0] MEM_to_D_forwardSignal;
 
-    wire        WB_RegWrite_signal;
-    wire [31:0] WB_write_data;
-    wire [4:0]  WB_write_register;
-    wire [2:0]  WB_exception_signal;
-    wire [10:0] EX_control_signal;
-    // wire [4:0] D_Reg_address1 = D_instruction[25:21], D_Reg_address2 = D_instruction[20:16];
-    // wire [31:0] D_REG_data_out1, D_REG_data_out2;
-    reg [31:0] testt_reg_add;
-    wire [31:0]testt_reg;
-
+    wire [26:0] SYS_leds;
     //test
-   system sy(
+   system #(.divisor(1)) sy(
         //INPUT, giữ nguyên
-        .SYS_clk                (SYS_clk),
+        .clk                (SYS_clk),
         .SYS_reset              (SYS_reset),
         .SYS_output_sel         (SYS_output_sel), //trong �'�? l�  7 bit nhưng chỉ cần 3 bit l�  �'ủ hiện thực
-        .testt_reg_add          (testt_reg_add),
 
         //OUTPUT
-        // .PC (F_PC),
-        .F_instruction          (F_instruction),
-        .D_instruction          (D_instruction),
-        .EX_instruction         (EX_instruction),
-        .MEM_instruction        (MEM_instruction),
-        .WB_instruction         (WB_instruction),
-
-        // .D_stall                (D_stall),
-        .WB_RegWrite_signal     (WB_RegWrite_signal),
-        .WB_write_data          (WB_write_data),
-        .WB_write_register      (WB_write_register),
-        .WB_exception_signal    (WB_exception_signal),
-        .EX_control_signal      (EX_control_signal),
-        // .D_REG_data_out1        (D_REG_data_out1),
-        // .D_REG_data_out2        (D_REG_data_out2),
-        // .MEM_to_D_forwardSignal (MEM_to_D_forwardSignal),
-
-        .testt_reg              (testt_reg)
-
+        .PC(PC),
+        .SYS_leds (SYS_leds),
+        .CLK_led (CLK_led)
     );
     initial begin
         SYS_clk=0;
@@ -78,11 +49,11 @@ module sys_tb();
     initial
         begin
              //ki?m tra gi? tr? thanh ghi s? 8
-            testt_reg_add = 8;
+            SYS_output_sel = 7;
+
             SYS_reset = 0;
-            SYS_output_sel = 2;
             #1 SYS_reset = 1;
-            #2 SYS_reset = 0;
+            #10 SYS_reset = 0;
         end 
-    initial #250 $finish;
+    // initial #250 $finish;
 endmodule
